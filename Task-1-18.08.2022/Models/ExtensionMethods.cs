a80b00cb89770147a29b0dcc3e761c96867642dd
Ibrahim Aliyev <ibrahimma@code.edu.az>
2022-08-18 18:38:53 +0400

    Task-1

---
+++ Task-1-18.08.2022/Models/ExtensionMethods.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_1_18._08._2022.Models
+{
+    class ExtensionMethods
+    {
+        private static int[] _indexes;
+        public static bool isOdd(int num)
+        {
+            if (num % 2 != 0) return true;
+            return false;
+        }
+        public static bool isEven(int num)
+        {
+            if (num % 2 == 0) return true;
+            return false;
+        }
+        public static bool isContainsDigit(string str)
+        {
+            foreach (var item in str)
+            {
+                if (char.IsDigit(item)) return true;
+            }
+            return false;
+        }
+        public static string toCapitalize(string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(str.ToUpper());
+            return sb.ToString();
+
+        }
+
+        public static int[] GetValueIndexes(string str, char a)
+        {
+            int index = 0;
+            int[] arr = new int[0];
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == a)
+                {
+                    Array.Resize(ref arr, arr.Length + 1);
+                    arr[index] = i;
+                    index++;
+                    
+                }
+                    
+            }
+            return arr;
+        }
+    }
+}
---
+++ Task-1-18.08.2022/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Task_1_18._08._2022.Models;
+
+namespace Task_1_18._08._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ExtensionMethods.isOdd(10));
+            Console.WriteLine(ExtensionMethods.isEven(8));
+            Console.WriteLine(ExtensionMethods.isContainsDigit("Jhskaix9382nsk"));
+            Console.WriteLine(ExtensionMethods.toCapitalize("hjOhjKnfFfFghHHjkk"));
+            foreach (var item in ExtensionMethods.GetValueIndexes("This method searches the string for a certain character", 'a'))
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
